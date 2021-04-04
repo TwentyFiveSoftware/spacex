78e2161f26301de99007f2fd263368f35ff3728a
Sebastian Loose <sebastian.loose@yahoo.de>
2021-04-04 21:54:27 +0200

    Add props and colors to launch container

--- src/components/LaunchContainer.tsx
+++ src/components/LaunchContainer.tsx
@@ -1,19 +1,41 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
 import styles from '../styles/LaunchContainer.module.scss';
 
-const LaunchContainer = () => {
+type Params = {
+    active: boolean;
+    flightNumber: number;
+    name: string;
+    rocketName: string;
+    launchPadName: string;
+    payload: string;
+    date: number;
+};
+
+
+
+const formatDate = (date: number) => {
+    const DATE_TIME_FORMAT = Intl.DateTimeFormat('de', {
+        day: '2-digit',
+        month: '2-digit',
+        year: 'numeric',
+    });
+
+    return DATE_TIME_FORMAT.format(new Date(date * 1000))
+}
+
+const LaunchContainer: FunctionComponent<Params> = ({active, flightNumber, name, rocketName, launchPadName, payload, date}) => {
     return <div className={styles.container}>
-        <p className={styles.flightNumber}>#121</p>
-        <p className={styles.name}>Starlink-22</p>
+        <p className={styles.flightNumber}>#{flightNumber.toString()}</p>
+        <p className={styles.name}>{name}</p>
         <div className={styles.flexRow}>
-            <p className={styles.date}>24.03.2021</p>
+            <p className={styles.date}>{formatDate(date)}</p>
             <p className={styles.spacer}>•</p>
-            <p className={styles.rocketName}>Falcon 9</p>
+            <p className={styles.rocketName}>{rocketName}</p>
         </div>
         <div className={styles.flexRow}>
-            <p className={styles.launchPadName}>CCSFS SLC 40</p>
+            <p className={styles.launchPadName}>{launchPadName}</p>
             <p className={styles.spacer}>•</p>
-            <p className={styles.type}>Satellite</p>
+            <p className={styles.type}>{payload}</p>
         </div>
     </div>;
 };
--- src/components/SideBar.tsx
+++ src/components/SideBar.tsx
@@ -1,14 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
+import { UpcomingLaunchesContext } from '../App';
 import styles from '../styles/SideBar.module.scss';
 import LaunchContainer from './LaunchContainer';
 
 const SideBar = () => {
-    return <div className={styles.container}>
-        <div className={styles.launchTypSelection}></div>
-        <div className={styles.launchSelection}>
-            <LaunchContainer/>
+    const launches = useContext(UpcomingLaunchesContext);
+    return (
+        <div className={styles.container}>
+            <div className={styles.launchTypSelection}></div>
+            <div className={styles.launchSelection}>
+                {launches.map(obj => (
+                    <LaunchContainer
+                        active={false}
+                        flightNumber={obj.flight_number}
+                        name={obj.name}
+                        launchPadName={obj.launchpad?.name}
+                        rocketName={obj.rocket?.name}
+                        payload={obj.payloads[0]?.type}
+                        date={obj.date_unix}
+                    />
+                ))}
+            </div>
         </div>
-    </div>;
+    );
 };
 
 export default SideBar;
