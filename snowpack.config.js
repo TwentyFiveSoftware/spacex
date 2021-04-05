467136217f0480f428fac5219e3ba2a5f57905fd
TwentyFive <TwentyFiveSoftware@gmail.com>
2021-04-05 17:15:00 +0200

    add snowpack-webpack plugin to optimize the build files

--- snowpack.config.js
+++ snowpack.config.js
@@ -9,6 +9,43 @@ module.exports = {
         '@snowpack/plugin-dotenv',
         '@snowpack/plugin-typescript',
         '@snowpack/plugin-sass',
+        [
+            '@snowpack/plugin-webpack',
+            {
+                extendConfig: config => {
+                    delete config.optimization.splitChunks;
+                    delete config.optimization.runtimeChunk;
+
+                    config.module.rules[0] = {
+                        test: /\.js$/,
+                        exclude: /node_modules/,
+                        use: [
+                            {
+                                loader: 'babel-loader',
+                                options: {
+                                    presets: [
+                                        [
+                                            '@babel/preset-env',
+                                            {
+                                                bugfixes: true,
+                                                modules: false,
+                                                useBuiltIns: 'usage',
+                                                corejs: 3,
+                                            },
+                                        ],
+                                    ],
+                                    compact: true,
+                                },
+                            },
+                            { loader: '@snowpack/plugin-webpack/plugins/import-meta-fix.js' },
+                            { loader: '@snowpack/plugin-webpack/plugins/proxy-import-resolve.js' },
+                        ],
+                    };
+
+                    return config;
+                },
+            },
+        ],
     ],
     routes: [],
     optimize: {
